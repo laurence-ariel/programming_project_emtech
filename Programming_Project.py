f7560930a72a59a2ca222e680bd13fb6fa75b903
Ariel Laurence Cunanan <alcunanan@Lances-MacBook-Pro.local>
2026-03-17 08:28:43 +0800

    just rewrote a few lines

--- Programming_Project.py
+++ Programming_Project.py
@@ -1,44 +1,39 @@
-class Hacker:
-    def __init__(self, name = input("Enter Player Hacker's Name:"), hp = 100, en = 100):
-        if name == "":
-            self.name = "Neo"
-            self.hp = hp
-            self.en = en
-        else:
-            self.name = name
+class Hacker: # Phase 0: values  and attacks init
+    def __init__(self, name = "Neo", hp = 100, en = 30):
+        self.name = name if name != "" else "Neo"
+        self.hp = hp
+        self.en = en # capped at 100, starts at 30
 
-    def ddosAttack(): # if hacker types in 1
+    def ddosAttack(self): # if hacker types in 1
         self.en -= 15
         #todo: Inflicts 20 dmg to the SysAdmin
 
-    def phishingScam(): # if hacker types in 2
+    def phishingScam(self): # if hacker types in 2
         self.hp -= 10
         self.en += 20
         #todo: restores 20 energy (capped at 100)
 
-    def stealthMode(): # if hacker types in 3
+    def stealthMode(self): # if hacker types in 3
         self.en -= 10
         #todo: Blocks SysAdmin's Firewall Purge
 
 class SysAdmin:
-    def __init__(self, name = input("Enter Player SysAdmin's Name:"), hp = 100, en = 100):
-        if name == "":
-            self.name = "Agent Smith"
-            self.hp = hp
-            self.en = en
-        else:
-            self.name = name
+    def __init__(self, name = "Agent Smith", hp = 100, en = 30):
+        self.name = name if name != "" else "Agent Smith"
+        self.hp = hp
+        self.en = en # capped at 100, starts at 30
 
-    def firewallPurge(): # if sysadmin types in 1
+
+    def firewallPurge(self): # if sysadmin types in 1
         self.en -= 15
         #todo: Inflicts 20 dmg to the Hacker
 
-    def rebootSystem(): # if sysadmin types in 2
+    def rebootSystem(self): # if sysadmin types in 2
         self.hp -= 10
         self.en += 20
         #todo: restores 20 energy (capped at 100)
 
-    def traceRoute(): # if sysadmin types in 3
+    def traceRoute(self): # if sysadmin types in 3
         self.en -= 10
         #todo: inflicts 10 dmg; bypasses Stealth
 
@@ -46,6 +41,10 @@ class SysAdmin:
 
 if __name__ == "__main__":
     print("placeholder entry screen")
-    hacker = Hacker() #todo: name prompt, hp and energy default to 100, 3 digit number valid input
-    sysadmin = SysAdmin()
     # todo: main game loop, prompt for input, call functions based on input, check for win conditions, print status updates
+    game_running = True
+    while game_running:
+        hacker = Hacker()
+        sysadmin = SysAdmin()
+
+        print(f"Hacker: {hacker.name}, Hacker HP: {hacker.hp}, SysAdmin: {sysadmin.name}")
